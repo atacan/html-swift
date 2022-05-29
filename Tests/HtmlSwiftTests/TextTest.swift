4742117e0424be95630621354eedfe0b4b1b5cda
Atacan Durmusoglu <atacan@atacans-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-05-29 15:23:21 +0200

    first tests

---
+++ Tests/HtmlSwiftTests/TextTest.swift
@@ -0,0 +1,28 @@
+// https://github.com/atacan/
+// 29.05.22
+//
+    
+
+import XCTest
+@testable import HtmlSwift
+
+final class TestText: XCTestCase {
+    func testDataCellIconText() throws {
+        
+        let htmlCode = #"""
+<td>
+    <i class="folder icon"></i> Test
+</td>
+"""#
+        
+        let targetSwiftCode = #"""
+Td {
+    I().class("folder icon")
+    Text("Test")
+}
+"""#
+        
+        let outputSwiftCode = try Converter.default.convert(html: htmlCode)
+        XCTAssertEqual(outputSwiftCode.removeWhitespace(), targetSwiftCode.removeWhitespace())
+    }
+}
