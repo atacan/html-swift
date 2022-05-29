28a4bd2f6ed0d2d83a1f2da06123b5cf05797b51
Atacan Durmusoglu <atacan@atacans-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-05-29 15:23:21 +0200

    first tests

---
+++ Sources/HtmlSwift/Helpers/Protocols.swift
@@ -0,0 +1,7 @@
+// https://github.com/atacan/
+// 28.05.22
+    
+
+protocol HaveInit {
+    init()
+}
---
+++ Tests/HtmlSwiftTests/BasicExampleTest.swift
@@ -0,0 +1,79 @@
+// https://github.com/atacan/
+// 29.05.22
+//
+    
+import SwiftHtml
+import XCTest
+@testable import HtmlSwift
+
+
+final class GeneralTests: XCTestCase {
+    func testReadMeExample() throws {
+        let swiftCode = """
+Html {
+    Head {
+        Title("Hello Swift HTML DSL")
+        
+        Meta().charset("utf-8")
+        Meta().name(.viewport).content("width=device-width, initial-scale=1")
+
+        Link(rel: .stylesheet).href("./css/style.css")
+    }
+    Body {
+        Main {
+            Div {
+                Section {
+                    Img(src: "./images/swift.png", alt: "Swift Logo")
+                        .title("Picture of the Swift Logo")
+                    H1("Lorem ipsum")
+                        .class("red")
+                    P("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                        .class("green blue")
+                        .spellcheck(false)
+                }
+
+                A("Download SwiftHtml now!")
+                    .href("https://github.com/binarybirds/swift-html/")
+                    .target(.blank)
+                    
+                Abbr("WTFPL")
+                    .title("Do What The Fuck You Want To Public License")
+            }
+        }
+        .class("container")
+
+        Script().src("./js/main.js")
+    }
+}
+"""
+        
+        let html = """
+<html>
+  <head>
+    <title>Hello Swift HTML DSL</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="./css/style.css"/>
+  </head>
+  <body>
+    <main class="container">
+      <div>
+        <section>
+          <img src="./images/swift.png" alt="Swift Logo" title="Picture of the Swift Logo"/>
+          <h1 class="red">Lorem ipsum</h1>
+          <p class="green blue" spellcheck="false">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </section>
+        <a href="https://github.com/binarybirds/swift-html/" target="_blank">Download SwiftHtml now!</a>
+        <abbr title="Do What The Fuck You Want To Public License">WTFPL</abbr>
+      </div>
+    </main>
+    <script src="./js/main.js"></script>
+  </body>
+</html>
+"""
+
+        let swiftConverted = try Converter.default.convert(html: html)
+        XCTAssertEqual(swiftCode.removeWhitespace(), swiftConverted.removeWhitespace())
+    }
+}
+
