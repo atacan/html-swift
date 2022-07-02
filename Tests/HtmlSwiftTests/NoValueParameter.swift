0d414fac2fc54719b25173ccb02c986e7beecd66
Atacan Durmusoglu <atacan@atacans-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-07-03 00:39:02 +0200

    async attribute with regex fix

--- Tests/HtmlSwiftTests/NoValueParameter.swift
+++ Tests/HtmlSwiftTests/NoValueParameter.swift
@@ -3,4 +3,26 @@
 // 03.07.22
 	
 
-import Foundation
+@testable import HtmlSwift
+import XCTest
+
+final class TestNoValueParameters: XCTestCase {
+    func testNoValueParameters() throws {
+        let htmlCode = #"""
+        <html>
+        <head>
+            <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+                crossorigin="anonymous"></script>
+            <title>Document</title>
+        </head>
+        <body>
+            <script src='https://www.google-analytics.com/analytics.js' async></script>
+        </body>
+        </html>
+        """#
+
+        XCTAssertNoThrow(try Converter.default.convert(html: htmlCode))
+        XCTAssertNoThrow(try Converter.default.convert(html: htmlCode.condensingWhitespace()))
+    }
+}
